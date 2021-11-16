aa25e452dea0788ecc2d82e368744a4db0e14db8
haleyej <haleyej@umich.edu>
2021-11-16 13:12:40 -0500

    javascript for page

--- javascript/loader.js
+++ javascript/loader.js
@@ -1,7 +1,9 @@
+// last for 1.25 seconds
 window.addEventListener('load', function(){
     myVar = setTimeout(showPage, 1250)
 })
 
+// function to show loader and then display page
 function showPage(){
     console.log("entering load")
     document.getElementById("loader").style.display = "none"
